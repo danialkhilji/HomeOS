b7fb09302f908e041f155ce978178f51d1828133
Muhammad Danial Khilji <danialkhilji@yahoo.com>
2026-08-10 17:35:44 +0100

    Remove pencil edit icon from shopping list, long-press replaces it

--- frontend/src/features/shopping/ShoppingList.tsx
+++ frontend/src/features/shopping/ShoppingList.tsx
@@ -10,15 +10,6 @@ interface ShoppingListProps {
   onDelete: (id: number) => void;
 }
 
-function PencilIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
-      <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-      <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
-    </svg>
-  );
-}
-
 function TrashIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
@@ -85,11 +76,6 @@ function ShoppingRow({ item, onToggle, onEdit, onDelete }: { item: ShoppingItem;
         </p>
       </div>
 
-      <IconButton
-        icon={<PencilIcon />}
-        label={`Edit ${item.name}`}
-        onClick={onEdit}
-      />
       <IconButton
         icon={<TrashIcon />}
         variant="danger"
